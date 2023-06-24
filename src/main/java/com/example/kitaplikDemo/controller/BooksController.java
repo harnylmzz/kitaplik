ff4b53dd279f6039e10bd4446fe274c4e3838d61
harnylmzz <harnylmzzonline@gmail.com>
2023-06-24 17:36:50 +0300

    bookId

--- src/main/java/com/example/kitaplikDemo/controller/BooksController.java
+++ src/main/java/com/example/kitaplikDemo/controller/BooksController.java
@@ -1,10 +1,32 @@
 package com.example.kitaplikDemo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.kitaplikDemo.business.abstracts.BookService;
+import com.example.kitaplikDemo.dto.responses.GetAllBookResponses;
+import com.example.kitaplikDemo.model.Book;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
-@RequestMapping
+@RequestMapping("/books")
 public class BooksController {
     
+    private BookService bookService;
+
+    @GetMapping("/getall")
+    public List<GetAllBookResponses> getAll() {
+        return this.bookService.getAllBooks();
+    }
+
+    @GetMapping("/{bookId}")
+    public Book getOneBook (@PathVariable Long bookId) {
+        return this.bookService.getOneBook(bookId);
+    }
 }
