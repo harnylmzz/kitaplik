6ed800357e95e238a434ce622ec25ce89fab200f
harnylmzz <harnylmzzonline@gmail.com>
2023-07-01 19:05:04 +0300

    repository

--- src/main/java/com/example/kitaplikDemo/repository/BookRepository.java
+++ src/main/java/com/example/kitaplikDemo/repository/BookRepository.java
@@ -7,11 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.kitaplikDemo.model.Book;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
-    
-        boolean existsByBookName(String bookName);
-        boolean existsByAuthorName(String authorName);
-        boolean existsByAuthorNameOrSurname(String authorName, String authorSurname);
-        boolean existsByAuthorNameAndSurname(String authorName, String authorSurname);
+
         List<Book> findByBookNameStartingWith(String bookName);
         List<Book> findByAuthorNameStartingWith(String authorName);
         
