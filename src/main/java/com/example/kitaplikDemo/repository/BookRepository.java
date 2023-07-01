99c69a386908cf31a5b60f952282cb0a3020a023
harnylmzz <harnylmzzonline@gmail.com>
2023-07-01 14:49:33 +0300

    commit

--- src/main/java/com/example/kitaplikDemo/repository/BookRepository.java
+++ src/main/java/com/example/kitaplikDemo/repository/BookRepository.java
@@ -1,9 +1,18 @@
 package com.example.kitaplikDemo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.kitaplikDemo.model.Book;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     
+        boolean existsByBookName(String bookName);
+        boolean existsByAuthorName(String authorName);
+        boolean existsByAuthorNameOrSurname(String authorName, String authorSurname);
+        boolean existsByAuthorNameAndSurname(String authorName, String authorSurname);
+        List<Book> findByBookNameStartingWith(String bookName);
+        List<Book> findByAuthorNameStartingWith(String authorName);
+
 }
--- src/main/java/com/example/kitaplikDemo/repository/UserRepository.java
+++ src/main/java/com/example/kitaplikDemo/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.kitaplikDemo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.kitaplikDemo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
+    boolean existsByUserName(String userName);
+    List<User> findByUserNameStartingWith(String userName);    
 }
