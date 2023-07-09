4ced509dfa46bea917f3a28be377d6a803d3e50f
harnylmzz <harnylmzzonline@gmail.com>
2023-07-09 08:43:41 +0300

    languagerepo

---
+++ src/main/java/com/example/kitaplikDemo/repository/LanguageRepository.java
@@ -0,0 +1,9 @@
+package com.example.kitaplikDemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.kitaplikDemo.model.Language;
+
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+    
+}
